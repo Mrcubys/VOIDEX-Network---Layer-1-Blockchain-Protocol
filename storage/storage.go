c073d4cf07f2b0b5fcfaf2b2a33c73bc0d7bbd0b
Voidex Network <shin85816@gmail.com>
2026-02-23 23:37:29 +0700

    Add Storage interface for database operations

---
+++ storage/storage.go
@@ -0,0 +1,33 @@
+package storage
+
+import "github.com/Mrcubys/VOIDEX-Network/core"
+
+// Storage interface defines database operations
+type Storage interface {
+	// Block operations
+	StoreBlock(block *core.Block) error
+	GetBlock(height uint64) (*core.Block, error)
+	GetBlockByHash(hash string) (*core.Block, error)
+	DeleteBlock(height uint64) error
+
+	// UTXO operations
+	StoreUTXO(utxo *core.UTXO) error
+	GetUTXO(txHash string, outIndex uint32) (*core.UTXO, error)
+	DeleteUTXO(txHash string, outIndex uint32) error
+	GetAllUTXOs() ([]*core.UTXO, error)
+
+	// Transaction operations
+	StoreTx(tx *core.Transaction) error
+	GetTx(txHash string) (*core.Transaction, error)
+	GetTxsByAddress(address string) ([]*core.Transaction, error)
+
+	// State operations
+	StoreState(key string, value []byte) error
+	GetState(key string) ([]byte, error)
+	DeleteState(key string) error
+
+	// Maintenance
+	Close() error
+	Backup() error
+	GetStats() map[string]interface{}
+}
